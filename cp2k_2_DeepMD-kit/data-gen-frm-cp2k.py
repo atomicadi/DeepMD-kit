6e4405600f5c6d628f5a513ba32fe36ddeb52012
Aditya Barman <147025377+atomicadi@users.noreply.github.com>
2024-11-04 18:33:43 +0530

    data-gen-frm-cp2k.py

--- cp2k_2_DeepMD-kit/data-gen-frm-cp2k.py
+++ cp2k_2_DeepMD-kit/data-gen-frm-cp2k.py
@@ -2,12 +2,12 @@
 from cp2kdata import Cp2kOutput
 import numpy as np
 
-cp2kmd_dir = input("Please, enter the folder name: ")
+cp2kmd_dir = input("Please, enter the output folder name: ")
 cp2kmd_output_name = input("Please, enter the log file name: ")
 dp = dpdata.LabeledSystem(cp2kmd_dir, cp2k_output_name=cp2kmd_output_name, fmt="cp2kdata/md")
 print(dp)
 
-n = int(input("Please, write the number of frames for training: "))
+n = int(input("Please, enter the number of frames for training: "))
 # random choose for training_data
 index_training = np.random.choice(len(dp), size=n, replace=False)
 # other indexes are validation_data
